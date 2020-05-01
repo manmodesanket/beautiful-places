1d7dd16f887f1735a1be55edd7fc9911054da8b3
Sanket Manmode <manmodesanket@gmail.com>
2020-05-01 19:15:52 +0530

    setup done and navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navitem">
+        <h3>Beautiful Places</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
