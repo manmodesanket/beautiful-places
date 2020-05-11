29ddb4a41a2a899dd4665ccbd76131ac3674d2cb
Sanket Manmode <manmodesanket@gmail.com>
2020-05-11 18:17:40 +0530

    link problem solved

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Add from "./components/Add";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 import "./css/navbar.css";
 import "./css/styles.css";
 
 const App = () => {
   return (
     <div>
-      <nav className="navbar">
-        <div className="navitem">
-          <Link to="/">
-            <h1>Beautiful Places</h1>
-          </Link>
-        </div>
-        <div className="navitem">
-          {" "}
-          <Link to="/add">
-            <button className="btn">Add a Place</button>
-          </Link>
-        </div>
-      </nav>
+      <Navbar />
       <Router>
         <Home path="/" />
         <Add path="/add" />
