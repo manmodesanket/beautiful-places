8705cec34a6396e5f8d5ebb6c5c6ae96614650f1
Sanket Manmode <manmodesanket@gmail.com>
2020-05-13 10:50:45 +0530

    alignment of navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,18 @@ import "../css/navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navitem">
-        <Link to="/">
-          <h1>Beautiful Places</h1>
-        </Link>
-      </div>
-      <div className="navitem">
-        {" "}
-        <Link to="/add">
-          <button className="btn">Add a Place</button>
-        </Link>
+      <div className="items">
+        <div className="navitem">
+          <Link to="/">
+            <h1>Beautiful Places</h1>
+          </Link>
+        </div>
+        <div className="navitem">
+          {" "}
+          <Link to="/add">
+            <button className="btn">Add a Place</button>
+          </Link>
+        </div>
       </div>
     </nav>
   );
